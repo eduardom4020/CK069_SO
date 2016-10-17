3005a64c5b605f9b748e3ac1ccf4ddf21c96c364
Eduardo <eduardom4020@gmail.com>
2016-10-17 14:08:22 -0300

    lab 5

---
+++ CK069_367089_LAB5_Escalonamentos/src/FileManager.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+
+
+public class FileManager 
+{
+	private static ArrayList<String> lines;
+	
+	public static ArrayList<String> readFile(String file_name) throws Exception		//fileNotFound e IO
+	{
+		lines = new ArrayList<String>();		
+		BufferedReader buffered_reader = new BufferedReader(new FileReader(file_name+".csv"));
+
+		while(buffered_reader.ready())
+		{
+			lines.add(buffered_reader.readLine());
+		}
+		
+		buffered_reader.close();
+		
+		return lines;
+	}
+	
+	public static void writeFile(String file_name, ArrayList<String> lines_in) throws IOException
+	{
+		lines = lines_in;		
+		BufferedWriter buffered_writer = new BufferedWriter(new FileWriter(file_name+".csv"));
+		
+		for(int i=0; i<lines.size(); i++)
+		{
+			buffered_writer.write(lines.get(i));
+			buffered_writer.newLine();
+		}
+		
+		buffered_writer.close();
+	}
+}
