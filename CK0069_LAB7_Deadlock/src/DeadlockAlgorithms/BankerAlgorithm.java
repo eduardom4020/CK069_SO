246ab8026ac7bd005f4572d1af92e93a32d28e3e
Eduardo <eduardom4020@gmail.com>
2016-10-26 12:06:24 -0300

    fazer o main

---
+++ CK0069_LAB7_Deadlock/src/DeadlockAlgorithms/BankerAlgorithm.java
@@ -0,0 +1,89 @@
+package DeadlockAlgorithms;
+
+import javax.print.attribute.standard.Finishings;
+
+import ResourcesManaging.ResourcesManager;
+
+public class BankerAlgorithm 
+{
+	private ResourcesManager rm = null;
+	
+	public BankerAlgorithm(String file_name)
+	{
+		rm = new ResourcesManager(file_name);
+	}
+	
+	public boolean safety()
+	{
+		int process_qtd = rm.n;
+		int resources_types = rm.m;
+		
+		for(int i=0; i<process_qtd; i++)
+		{
+			for(int j=0; j<resources_types; j++)
+			{
+			//se o processo i precisa de mais instancias do recurso j que as disponiveis entao temos um deadlock (nao estah seguro)
+				if(rm.Need.get(i).get(j) > rm.Avaliable.get(j))	
+				{
+					return false;
+				}
+			}
+		}
+		
+		return true;
+	}
+
+//um processo solicita mais recursos. Se ao alocar esses recursos solicitados, ele ainda estiver em um estado seguro, a operacao eh aceita
+//se nao, a operacao eh desfeita.
+	public boolean avoid(String request)
+	{
+		rm.allocate(request);
+		
+		if(safety())
+		{
+			return true;
+		}
+		else
+		{
+			rm.block(request);
+			
+			return false;
+		}
+	}
+	
+//confere se estah em estado seguro. Faz a requisicao do need de p0. Se for possivel, repetimos o processo para p1.
+//Se nao for possivel, bloqueamos p0 e tentamos em p1. Quando o need de um processo for zero, este eh terminado e adicionado a 
+//lista de finish.
+	public ArrayList<Integer> detection()
+	{		
+		for(int j=0; j<rm.n; j++)
+		{
+			String request = String.valueOf(j);
+			
+			for(int i=0; i<rm.m; i++)
+			{
+				request += " " + rm.Need.get(j).get(i);
+			}
+			
+			
+			if(avoid(request))
+			{
+				rm.finish(j);
+			}
+		}
+		
+		while(rm.RequestBlocked.size() > 0)
+		{
+			String request_blocked = rm.RequestBlocked.remove(0);
+			
+			if(avoid(request_blocked))
+			{
+				String[] tokens = request_blocked.split(" ");
+				
+				rm.finish(Integer.parseInt(tokens[0]));
+			}
+		}
+		
+		return rm.Finished;
+	}
+}
--- CK0069_LAB7_Deadlock/src/ResourcesManaging/ResourcesManager.java
+++ CK0069_LAB7_Deadlock/src/ResourcesManaging/ResourcesManager.java
@@ -2,29 +2,44 @@ package ResourcesManaging;
 
 import java.util.ArrayList;
 
+import com.sun.xml.internal.bind.v2.runtime.unmarshaller.XsiNilLoader.Array;
+
 import FileManaging.FileManager;
 
 public class ResourcesManager extends FileManager
 {
+//os dois vetores abaixo representam os tipos de recursos e suas instancias. Cada posicao do vetor eh um tipo de recurso diferente
+//e seu valor eh a quantidade de instancias
+	public ArrayList<Integer> Avaliable = null;				//vetor que indica o quantas instâncias de recurso estao disponiveis
+	public ArrayList<Integer> Instances = null;				//vetor que indica a quantidade de instancias por tipo de recurso
 	
-	public ArrayList<Integer> Avaliable = null;
-	public ArrayList<ArrayList<Integer>> Max = null;
-	public ArrayList<ArrayList<Integer>> Allocation = null;
-	public ArrayList<ArrayList<Integer>> Need = null;
+//as matrizes abaixo representam a relacao entre os processos e os recursos. Cada linha da matriz eh um processo e cada coluna eh
+//um tipo de recurso
+	public ArrayList<ArrayList<Integer>> Max = null;		//a capacidade maxima de instancias de tipos de recursos que um processo aguenta
+	public ArrayList<ArrayList<Integer>> Allocation = null;	//a quantidade de instancias de recursos que estao sendo fornecidas para os processos
+	public ArrayList<ArrayList<Integer>> Need = null;		//Need = Max - Allocation. O quanto ainda falta para liberar o processo.
 	
-	public int n;
-	public int m;
+	public ArrayList<String> RequestBlocked = null;
+	
+	public ArrayList<String> Finished = null;
+	
+	public int n;	//numero de processos
+	public int m;	//numero de tipos de recursos
 	
 	private ArrayList<String> lines = null;
 	private String actualLine = null;
-	private String[] tokens=null;
 	
 	public ResourcesManager(String file_name)
 	{
 		Avaliable = new ArrayList<>();
-		Max = new ArrayList< ArrayList<Integer> >();
-		Allocation = new ArrayList< ArrayList<Integer> >();
-		Need = new ArrayList< ArrayList<Integer> >();
+		Max = new ArrayList<>();
+		Allocation = new ArrayList<>();
+		Need = new ArrayList<>();
+		Instances = new ArrayList<>();
+		RequestBlocked = new ArrayList<>();
+		Finished = new ArrayList<>();
+		
+		int N=0;
 		
 		lines = readFile(file_name);
 		
@@ -32,30 +47,143 @@ public class ResourcesManager extends FileManager
 		{
 			actualLine = lines.get(i);
 			
-			tokens = actualLine.split(" ");
-			
+		//a primeira linha do arquivo deve conter a quantidade de processos e de tipos de recursos respectivamente
 			if(i==0)
 			{
-				n = Integer.parseInt(tokens[0]);
+				String[] tokens = actualLine.split(" ");
+				
+				N = Integer.parseInt(tokens[0]);
 				m = Integer.parseInt(tokens[1]);
+			}
+		//a quantidade de instancias para cada tipo de recurso. Os tipos de recurso devem ser iguais a m. Caso passe do valor 
+		//estes serao ignorados, caso nao alcance ocorrerah falha de segmentacao.
+			else if(i==1)
+			{
+				for(int j=0; j<m; j++)
+				{
+					String[] tokens = actualLine.split(" ");
+					
+					Instances.add(Integer.parseInt(tokens[j]));
+				}
 			}
 			else
 			{
-				for(int j=0; j<n; j++)
+				for(int j=0; j<N; j++)	//itera pela quantidade de processos
 				{
-					Allocation.add(new ArrayList<Integer>());
-					Max.add(new ArrayList<Integer>());
-					Need.add(new ArrayList<Integer>());
-					
-					for(int k=0; k<m; k++)
-					{
-						Allocation.get(j).add(Integer.parseInt(tokens[k]));
-						Max.get(j).add(Integer.parseInt(tokens[m+k]));
-						
-						Need.get(j).add(Max.get(j).get(k) - Allocation.get(j).get(k));
-					}
+					push_back(actualLine);
 				}
+				
+				setAvaliable();
 			}
 		}
+	}
+	
+//calculamos a quantidade de instancias disponiveis para um recurso
+	private void setAvaliable()
+	{
+		for(int j=0; j<m; j++)	//itera pela quantidade de tipos de recusos
+		{
+			int sum=0;
+			
+			for(int k=0; k<n; k++)	//itera pela quantidade de processos
+			{
+				sum += Allocation.get(n).get(m);
+			}
+			
+			Avaliable.add(Instances.get(m) - sum);
+		}
+	}
+	
+//adiciona um processo, com seus respectivos pedidos de recurso, ao gerente de recursos. No final da lista
+	public void push_back(String new_process)
+	{
+		String[] tokens = new_process.split(" ");
+		
+		Allocation.add(new ArrayList<Integer>());
+		Max.add(new ArrayList<Integer>());
+		Need.add(new ArrayList<Integer>());
+		
+		n = Allocation.size();
+		
+		for(int i=0; i<m; i++)
+		{
+			Allocation.get(n-1).add(Integer.parseInt(tokens[i]));
+			Max.get(n-1).add(Integer.parseInt(tokens[m+i]));
+			
+			Need.get(n-1).add(Max.get(n-1).get(i) - Allocation.get(n-1).get(i));
+		}
+		
+		setAvaliable();
+	}
+
+//remove o ultimo processo da lista de pedidos por recurso
+	public ArrayList<Integer> pop(int pid)
+	{
+		ArrayList<Integer> poped = new ArrayList<>();
+
+		for(int i=0; i<m; i++)
+		{
+			poped.add(Allocation.get(pid).get(i));
+			poped.add(Max.get(pid).get(m+i));
+		}
+		
+		Allocation.remove(pid);
+		Max.remove(pid);
+		Need.remove(pid);
+		
+		n -= 1;
+		
+		setAvaliable();
+		
+		return poped;
+	}
+	
+	public ArrayList<Integer> pop_back()
+	{
+		return pop(n-1);
+	}
+
+//um processo solicita mais recursos
+	public void allocate(String request)	//string do tipo:	pid		r1	r2	r3	... (onde r sao as quantidades alocadas para os recursos em ordem)
+	{
+		String[] tokens = request.split(" ");
+		
+		int pid = Integer.parseInt(tokens[0]);
+		
+		for(int i=0; i<m; i++)
+		{
+			Allocation.get(pid).set(i, Allocation.get(pid).get(i) + Integer.parseInt(tokens[i+1]));
+			Need.get(pid).set(i, Max.get(pid).get(i) - Allocation.get(pid).get(i));			
+		}
+	}
+	
+//remove a solicitacao feita
+	public void deallocate(String request)
+	{
+		String[] tokens = request.split(" ");
+		
+		int pid = Integer.parseInt(tokens[0]);
+		
+		for(int i=0; i<m; i++)
+		{
+			Allocation.get(pid).set(i, Allocation.get(pid).get(i) - Integer.parseInt(tokens[i+1]));
+			Need.get(pid).set(i, Max.get(pid).get(i) + Allocation.get(pid).get(i));			
+		}
+	}
+	
+//adiciona o pedido a lista de pedidos bloqueados. Consequentemente significando que o processo estah bloqueado
+	public void block(String request)
+	{
+		deallocate(request);
+		
+		RequestBlocked.add(request);
+	}
+	
+//finaliza um processo
+	public void finish(int pid)
+	{
+		Finished.add("p"+pid);
+		
+		pop(pid);
 	}
 }
